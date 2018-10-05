19ef88d9d9433e49f729525e9d271d176bda2a0c
alanmathew333 <mathewalan84@gmail.com>
2018-10-05 22:07:24 +0530

    docker command workaround

--- index.js
+++ index.js
@@ -11,14 +11,16 @@ const routes = require('./lib/commands/routeslib');
 const config = require('./lib/commands/configlib');
 const server = require('./lib/run/server');
 const client = require('./lib/run/client');
+const docker = require('./lib/container/docker');
 
-let initcommand = init.initfunction;
-let modelscommand = models.modelsfunction;
-let controllerscommand = controllers.controllersfunction;
-let routescommand = routes.routesfunction;
-let configcommand = config.configfunction;
-let runserver = server.serverfunction;
-let runclient = client.clientfunction;
+let initcmd = init.initfn;
+let modelscmd = models.modelsfn;
+let controllerscmd = controllers.controllersfn;
+let routescmd = routes.routesfn;
+let configcmd = config.configfn;
+let runserver = server.serverfn;
+let runclient = client.clientfn;
+let dockerize = docker.dockerfn;
 
 program
   .version('1.0.0', '-v --version')
@@ -27,27 +29,27 @@ program
 program
   .command('init <appname>')
   .description('To init the project')
-  .action(initcommand);
+  .action(initcmd);
 
 program
   .command('create:controller')
   .description('To create controllers-file')
-  .action(controllerscommand);
+  .action(controllerscmd);
 
 program
   .command('create:route')
   .description('To create router-file')
-  .action(routescommand);
+  .action(routescmd);
 
 program
   .command('create:config')
   .description('To create configuration file for database')
-  .action(configcommand);
+  .action(configcmd);
 
 program
   .command('create:model')
   .description('To create models-file')
-  .action(modelscommand);
+  .action(modelscmd);
 
 program
   .command('run:client')
@@ -58,5 +60,10 @@ program
   .command('run:server')
   .description('To run server')
   .action(runserver);
+
+program
+  .command('dockerize')
+  .description('To dockerize the app')
+  .action(dockerize)  
 
 program.parse(process.argv);
