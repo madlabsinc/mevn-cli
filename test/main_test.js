bdbecdf38902b7f8bb002b3906f0b3f698cfb65e
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-02-08 15:31:07 +0530

    Add test cases :rocket:

--- test/main_test.js
+++ test/main_test.js
@@ -3,16 +3,30 @@
 const path = require('path'),
 { exec } = require('child_process');
 
-const command = path.join(process.cwd(), 'bin/index.js');
+const rootCommand = path.join(process.cwd(), 'bin/index.js');
 
 describe('mevn', () => {
 
   it('executes without error', (done) => {
-    exec(command, (err) => {
+    exec(rootCommand, (err) => {
       if (err) {
         throw err;
       }
       done();
     });
   });
+
+  ['', '-h', '--help'].forEach( (args) => {
+    var suffix = args ? '"' + args + '"' : 'no arguments';
+    it('shows help when executed with ' + suffix, (done) => {
+      exec(rootCommand + ' ' + args, (err, stdout) => {
+        if (err) {
+          throw err;
+        }
+        stdout.match(/Usage:\s+mevn/);
+	done();
+      });
+    });
+  });
+
 });
