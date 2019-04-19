30eced0f0b56db91523e9eb2c4032c3ab9be3f12
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-04-19 23:58:33 +0530

    Use a better way to defer execution for dockerize command :construction:

--- src/commands/deploy/docker.js
+++ src/commands/deploy/docker.js
@@ -5,29 +5,29 @@ import os from 'os';
 import shell from 'shelljs';
 
 import { configFileExists } from '../../utils/messages';
+import { deferExec } from '../../utils/defer';
 import { showBanner } from '../../external/banner';
 import { validateInstallation } from '../../utils/validations';
 
-exports.dockerize = () => {
+exports.dockerize = async () => {
   showBanner();
 
-  setTimeout(async () => {
-    configFileExists();
-    await validateInstallation('docker');
+  await deferExec(100);
+  configFileExists();
+  await validateInstallation('docker');
 
-    // currently works on linux only
-    if (os.type() === 'Linux') {
-      console.log('\n');
-      shell.exec('sudo docker-compose up', err => {
-        if (err) {
-          console.log(chalk.red.bold('something went wrong!'));
-          process.exit(1);
-        } else {
-          console.log(
-            'You are all set...!\nserver:- localhost:9000\nclient:- localhost:8080',
-          );
-        }
-      });
-    }
-  }, 100);
+  // currently works on linux only
+  if (os.type() === 'Linux') {
+    console.log('\n');
+    shell.exec('sudo docker-compose up', err => {
+      if (err) {
+        console.log(chalk.red.bold('something went wrong!'));
+        process.exit(1);
+      } else {
+        console.log(
+          'You are all set...!\nserver:- localhost:9000\nclient:- localhost:8080',
+        );
+      }
+    });
+  }
 };
