935fd8659300a9bd236e05c4f8e3cfc7db9cd69f
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-04-19 12:48:46 +0530

    Improve serve command action-handler :construction:

--- src/commands/serve/setup.js
+++ src/commands/serve/setup.js
@@ -5,40 +5,40 @@ import shell from 'shelljs';
 
 import { appData } from '../../utils/projectConfig';
 import { configFileExists } from '../../utils/messages';
+import { deferExec } from '../../utils/defer';
 import { serveProject } from './launch';
 import { showBanner } from '../../external/banner';
 
 let projectTemplate;
 
-exports.setupProject = () => {
+exports.setupProject = async () => {
   showBanner();
   configFileExists();
 
-  setTimeout(() => {
-    inquirer
-      .prompt([
-        {
-          name: 'side',
-          type: 'list',
-          message: 'Choose from below',
-          choices: ['client', 'server'],
-        },
-      ])
-      .then(async choice => {
-        let launchCmd = choice.side === 'client' ? 'npm run dev' : 'npm start';
+  await deferExec(200);
+  inquirer
+    .prompt([
+      {
+        name: 'side',
+        type: 'list',
+        message: 'Choose from below',
+        choices: ['client', 'server'],
+      },
+    ])
+    .then(async choice => {
+      let launchCmd = choice.side === 'client' ? 'npm run dev' : 'npm start';
 
-        await appData().then(data => {
-          projectTemplate = data.template;
-        });
-
-        if (choice.side === 'client') {
-          if (projectTemplate !== 'Nuxt-js') {
-            shell.cd('client');
-          }
-        } else {
-          shell.cd('server');
-        }
-        serveProject(launchCmd, projectTemplate);
+      await appData().then(data => {
+        projectTemplate = data.template;
       });
-  }, 200);
+
+      if (choice.side === 'client') {
+        if (projectTemplate !== 'Nuxt-js') {
+          shell.cd('client');
+        }
+      } else {
+        shell.cd('server');
+      }
+      serveProject(launchCmd, projectTemplate);
+    });
 };
