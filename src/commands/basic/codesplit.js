664f1c80bf482f0aca5335f6cdbab4f29d58634a
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-04-19 23:16:47 +0530

    Use a better way to defer execution for codesplit command :construction:

--- src/commands/basic/codesplit.js
+++ src/commands/basic/codesplit.js
@@ -5,52 +5,50 @@ import fs from 'fs';
 import shell from 'shelljs';
 
 import { configFileExists } from '../../utils/messages';
+import { deferExec } from '../../utils/defer';
 import { showBanner } from '../../external/banner';
 
-exports.asyncRender = componentName => {
+exports.asyncRender = async componentName => {
   showBanner();
 
-  setTimeout(() => {
-    configFileExists();
+  await deferExec(200);
+  configFileExists();
 
-    shell.cd('client/src/router');
+  shell.cd('client/src/router');
 
-    let componentNotFound = true;
-    let routerFile = fs
-      .readFileSync('./index.js', 'utf8')
-      .toString()
-      .split('\n');
-    for (let index = 0; index < routerFile.length; index++) {
-      // Checking whether the routes file already has the respective component
-      if (routerFile[index].includes(`${componentName}`)) {
-        //  Validating whether the respective component is already imported dynamically
-        if (routerFile[index].includes(`import ${componentName}`)) {
-          routerFile[
-            index
-          ] = `const ${componentName} = () => import('@/components/${componentName}')`;
-          componentNotFound = false;
-          break;
-        } else {
-          console.log(
-            chalk.redBright(
-              `\n ${componentName} component is already imported dynamically!`,
-            ),
-          );
-          process.exit(1);
-        }
+  let componentNotFound = true;
+  let routerFile = fs
+    .readFileSync('./index.js', 'utf8')
+    .toString()
+    .split('\n');
+  for (let index = 0; index < routerFile.length; index++) {
+    // Checking whether the routes file already has the respective component
+    if (routerFile[index].includes(`${componentName}`)) {
+      //  Validating whether the respective component is already imported dynamically
+      if (routerFile[index].includes(`import ${componentName}`)) {
+        routerFile[
+          index
+        ] = `const ${componentName} = () => import('@/components/${componentName}')`;
+        componentNotFound = false;
+        break;
+      } else {
+        console.log(
+          chalk.redBright(
+            `\n ${componentName} component is already imported dynamically!`,
+          ),
+        );
+        process.exit(1);
       }
     }
-    if (!componentNotFound) {
-      fs.writeFile('./index.js', routerFile.join('\n'), err => {
-        if (err) {
-          throw err;
-        }
-      });
-    } else {
-      console.log(
-        chalk.yellowBright('\n Make sure that the component exists!'),
-      );
-      process.exit(1);
-    }
-  }, 100);
+  }
+  if (!componentNotFound) {
+    fs.writeFile('./index.js', routerFile.join('\n'), err => {
+      if (err) {
+        throw err;
+      }
+    });
+  } else {
+    console.log(chalk.yellowBright('\n Make sure that the component exists!'));
+    process.exit(1);
+  }
 };
