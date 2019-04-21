ced9d918e9d4006c7bbfd7536759839f177ecdb0
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-04-21 11:34:53 +0530

    Use a better way to defer execution for deploy command :construction:

--- src/commands/deploy/herokuDeploy.js
+++ src/commands/deploy/herokuDeploy.js
@@ -6,6 +6,7 @@ import shell from 'shelljs';
 
 import { appData } from '../../utils/projectConfig';
 import { configFileExists, dependencyNotInstalled } from '../../utils/messages';
+import { deferExec } from '../../utils/defer';
 import { showBanner } from '../../external/banner';
 
 // Determining host OS
@@ -33,63 +34,62 @@ const validateInstallation = async () => {
   }
 };
 
-exports.deploy = () => {
+exports.deploy = async () => {
   showBanner();
 
-  setTimeout(async () => {
-    configFileExists();
-    await validateInstallation('heroku');
+  deferExec(100);
+  configFileExists();
+  await validateInstallation('heroku');
 
-    let templateIsNuxt;
-    await appData().then(data => {
-      templateIsNuxt = data.template === 'Nuxt-js';
+  let templateIsNuxt;
+  await appData().then(data => {
+    templateIsNuxt = data.template === 'Nuxt-js';
+  });
+
+  let serverPath = templateIsNuxt ? `/${process.cwd()}/server` : '../server';
+  let moveCmd = os.type() === 'Windows_NT' ? 'move' : 'mv';
+
+  if (!templateIsNuxt) {
+    shell.cd('client');
+  }
+
+  const buildCommands = [
+    'npm install',
+    'npm run build',
+    `${moveCmd} /${process.cwd()}/dist ${serverPath}`,
+    `sudo docker login --username=_ --password=$(heroku auth:token) registry.heroku.com`,
+  ];
+  await Promise.all(
+    buildCommands.map(cmd => {
+      shell.exec(cmd, { silent: true });
+    }),
+  ).catch(err => {
+    throw err;
+  });
+  shell.echo('\n Creating a heroku app');
+  await shell.exec('heroku create');
+
+  inquirer
+    .prompt([
+      {
+        name: 'appName',
+        type: 'input',
+        message: 'Please enter the name of heroku app(url)',
+      },
+    ])
+    .then(async userInput => {
+      try {
+        await shell.exec(
+          `sudo heroku container:push web -a ${userInput.appName}`,
+        );
+        await shell.exec(
+          `sudo heroku container:release web -a ${userInput.appName}`,
+        );
+        await shell.exec(`heroku open -a ${userInput.appName}`);
+      } catch (err) {
+        throw err;
+      }
     });
-
-    let serverPath = templateIsNuxt ? `/${process.cwd()}/server` : '../server';
-    let moveCmd = os.type() === 'Windows_NT' ? 'move' : 'mv';
-
-    if (!templateIsNuxt) {
-      shell.cd('client');
-    }
-
-    const buildCommands = [
-      'npm install',
-      'npm run build',
-      `${moveCmd} /${process.cwd()}/dist ${serverPath}`,
-      `sudo docker login --username=_ --password=$(heroku auth:token) registry.heroku.com`,
-    ];
-    await Promise.all(
-      buildCommands.map(cmd => {
-        shell.exec(cmd, { silent: true });
-      }),
-    ).catch(err => {
-      throw err;
-    });
-    shell.echo('\n Creating a heroku app');
-    await shell.exec('heroku create');
-
-    inquirer
-      .prompt([
-        {
-          name: 'appName',
-          type: 'input',
-          message: 'Please enter the name of heroku app(url)',
-        },
-      ])
-      .then(async userInput => {
-        try {
-          await shell.exec(
-            `sudo heroku container:push web -a ${userInput.appName}`,
-          );
-          await shell.exec(
-            `sudo heroku container:release web -a ${userInput.appName}`,
-          );
-          await shell.exec(`heroku open -a ${userInput.appName}`);
-        } catch (err) {
-          throw err;
-        }
-      });
-  }, 100);
 };
 
 const installHerokuCLI = async () => {
