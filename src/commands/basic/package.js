1c95275ebeb76ced77804950570df014f060ebe9
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-04-22 21:01:24 +0530

    Minor improvements for add:package command :construction:

--- src/commands/basic/package.js
+++ src/commands/basic/package.js
@@ -1,10 +1,8 @@
 'use strict';
 
-import chalk from 'chalk';
-import cmd from 'node-cmd';
+import execa from 'execa';
 import fs from 'fs';
 import inquirer from 'inquirer';
-import shell from 'shelljs';
 
 import { configFileExists } from '../../utils/messages';
 import { deferExec } from '../../utils/defer';
@@ -16,180 +14,128 @@ let storeFile = fs.readFileSync(
   'utf8',
 );
 
+const installPackage = async packageToInstall => {
+  const fetchSpinner = new Spinner(`Installing ${packageToInstall} `);
+  fetchSpinner.start();
+
+  try {
+    await execa('npm', ['install', '--save', packageToInstall]);
+  } catch (err) {
+    fetchSpinner.fail(`Failed to install ${packageToInstall}`);
+    throw err;
+  }
+  fetchSpinner.succeed(`Successfully installed ${packageToInstall}`);
+};
+
 exports.addPackage = async () => {
   showBanner();
 
-  await deferExec(1000);
+  await deferExec(100);
   configFileExists();
   console.log('\n');
   let questions = [
     {
       type: 'list',
-      name: 'packages',
+      name: 'package',
       message: 'Which package do you want to install?',
       choices: ['vee-validate', 'axios', 'vuex', 'vuetify'],
     },
   ];
 
-  shell.cd('client');
+  process.chdir('client');
   inquirer.prompt(questions).then(async userChoice => {
-    let fetchSpinner = new Spinner(`Installing ${userChoice.packages} `);
-    fetchSpinner.start();
+    await installPackage(userChoice.package);
 
-    await deferExec(100);
-    if (userChoice.packages === 'vee-validate') {
-      cmd.get('npm install vee-validate --save-dev', err => {
-        fetchSpinner.stop();
-
+    if (userChoice.package === 'vuex') {
+      process.chdir('src');
+      // Getting the file content as an array where each line represents the corresponding element.
+      let config = fs
+        .readFileSync('main.js', 'utf8')
+        .toString()
+        .split('\n');
+      // Creates a new store.js file within the client/src directory.
+      fs.writeFile('store.js', storeFile, err => {
         if (err) {
-          console.log(
-            chalk.red.bold(
-              "Something went wrong. Couldn't install the required packages!",
-            ),
-          );
-          process.exit(1);
+          throw err;
         }
-
-        console.log(chalk.green.bold('Package added successfully'));
       });
-    } else if (userChoice.packages === 'axios') {
-      cmd.get('npm install axios --save', err => {
-        fetchSpinner.stop();
 
-        if (err) {
-          console.log(
-            chalk.red.bold(
-              "Something went wrong. Couldn't install the required packages!",
-            ),
-          );
-          process.exit(1);
+      let storeNotImported = true;
+      // Iterates through the array, assuming that there is a blank line between import statements and rest part of the code.
+      for (let index = 0; index < config.length; index++) {
+        if (config[index] === "import store from './store'") {
+          break;
         }
 
-        console.log(chalk.green.bold('Package added successfully'));
+        if (config[index] === '' && storeNotImported) {
+          config[index] = "import store from './store'";
+          storeNotImported = false;
+        }
+
+        // Searches for the line where Vue is getting instantiated
+        if (config[index] === 'new Vue({') {
+          // This is the first line after the creation of the Vue instance
+          let indexWithin = index + 1;
+          while (config[indexWithin] !== '})') {
+            // Terminating line of creating a Vue instance.
+            indexWithin++;
+          }
+          // Inserting store within the Vue instance
+          let tempVal = config[indexWithin - 1];
+          config[indexWithin - 1] = '  store,';
+          config[indexWithin] = tempVal;
+          config[indexWithin + 1] = '})';
+        }
+      }
+      let content = config.join('\n');
+      fs.writeFile('main.js', content, err => {
+        if (err) {
+          throw err;
+        }
       });
-    } else if (userChoice.packages === 'vuex') {
-      cmd.get('npm install vuex --save', err => {
-        fetchSpinner.stop();
+    } else if (userChoice.package === 'vuetify') {
+      process.chdir('src');
+      let config = fs
+        .readFileSync('main.js', 'utf8')
+        .toString()
+        .split('\n');
 
+      for (let index = 0; index < config.length; index++) {
+        if (config[index] === "import Vuetify from './vuetify'") {
+          break;
+        }
+
+        if (config[index] === '') {
+          config[index] = "import Vuetify from 'vuetify'";
+          config[index + 2] = 'Vue.use(Vuetify)';
+          break;
+        }
+      }
+
+      let content = config.join('\n');
+      fs.writeFile('main.js', content, err => {
         if (err) {
-          console.log(
-            chalk.red.bold(
-              "Something went wrong. Couldn't install the required packages!",
-            ),
-          );
-          process.exit(1);
+          throw err;
         }
-
-        shell.cd('src');
-        // Getting the file content as an array where each line represents the corresponding element.
-        let config = fs
-          .readFileSync('main.js', 'utf8')
-          .toString()
-          .split('\n');
-        // Creates a new store.js file within the client/src directory.
-        fs.writeFile('store.js', storeFile, err => {
-          if (err) {
-            throw err;
-          }
-        });
-
-        let storeNotImported = true;
-        // Iterates through the array, assuming that there is a blank line between import statements and rest part of the code.
-        for (let index = 0; index < config.length; index++) {
-          if (config[index] === "import store from './store'") {
-            break;
-          }
-
-          if (config[index] === '' && storeNotImported) {
-            config[index] = "import store from './store'";
-            storeNotImported = false;
-          }
-
-          // Searches for the line where Vue is getting instantiated
-          if (config[index] === 'new Vue({') {
-            // This is the first line after the creation of the Vue instance
-            let indexWithin = index + 1;
-            while (config[indexWithin] !== '})') {
-              // Terminating line of creating a Vue instance.
-              indexWithin++;
-            }
-            // Inserting store within the Vue instance
-            let tempVal = config[indexWithin - 1];
-            config[indexWithin - 1] = '  store,';
-            config[indexWithin] = tempVal;
-            config[indexWithin + 1] = '})';
-          }
-        }
-        let content = config.join('\n');
-        fs.writeFile('main.js', content, err => {
-          if (err) {
-            throw err;
-          }
-        });
-        console.log(
-          chalk.green.bold(
-            'Package added successfully. You can find a store file within the src directory!',
-          ),
-        );
       });
-    } else if (userChoice.packages === 'vuetify') {
-      cmd.get('npm install --save vuetify', err => {
-        fetchSpinner.stop();
 
+      let rootComponent = fs
+        .readFileSync('App.vue', 'utf8')
+        .toString()
+        .split('\n');
+
+      for (let index = 0; index < rootComponent.length; index++) {
+        if (rootComponent[index] === '<style>') {
+          rootComponent[index] = "<style src='vuetify/dist/vuetify.min.css'>";
+          break;
+        }
+      }
+
+      let rootContent = rootComponent.join('\n');
+      fs.writeFile('App.vue', rootContent, err => {
         if (err) {
-          console.log(
-            chalk.red.bold(
-              "Something went wrong. Couldn't install the required packages!",
-            ),
-          );
-          process.exit(1);
+          throw err;
         }
-
-        shell.cd('src');
-        let config = fs
-          .readFileSync('main.js', 'utf8')
-          .toString()
-          .split('\n');
-
-        for (let index = 0; index < config.length; index++) {
-          if (config[index] === "import Vuetify from './vuetify'") {
-            break;
-          }
-
-          if (config[index] === '') {
-            config[index] = "import Vuetify from 'vuetify'";
-            config[index + 2] = 'Vue.use(Vuetify)';
-            break;
-          }
-        }
-
-        let content = config.join('\n');
-        fs.writeFile('main.js', content, err => {
-          if (err) {
-            throw err;
-          }
-        });
-
-        let rootComponent = fs
-          .readFileSync('App.vue', 'utf8')
-          .toString()
-          .split('\n');
-
-        for (let index = 0; index < rootComponent.length; index++) {
-          if (rootComponent[index] === '<style>') {
-            rootComponent[index] = "<style src='vuetify/dist/vuetify.min.css'>";
-            break;
-          }
-        }
-
-        let rootContent = rootComponent.join('\n');
-        fs.writeFile('App.vue', rootContent, err => {
-          if (err) {
-            throw err;
-          }
-        });
-
-        console.log(chalk.green.bold('Package added successfully'));
       });
     }
   });
