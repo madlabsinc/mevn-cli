f506f014ce4e79f2780c832ef2f75d9b72f0afd4
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-03-21 21:37:48 +0530

    refactor: start out with revamped deploy experience implementation

--- src/commands/deploy/heroku.js
+++ src/commands/deploy/heroku.js
@@ -1,50 +1,9 @@
 'use strict';
 
 import execa from 'execa';
-import inquirer from 'inquirer';
+import fs from 'fs';
 
 import { validateInstallation } from '../../utils/validate';
-
-/**
- * Deploy to Heroku via Git integration
- *
- * @returns {Promise<void>}
- */
-
-const deployWithGit = async () => {
-  // Commands to be executed inorder to deploy the webapp to Heroku via Git integration.
-  try {
-    Promise.all([
-      await execa('heroku', ['login'], { stdio: 'inherit' }),
-      await execa('heroku', ['create'], { stdio: 'inherit' }),
-      await execa('git', ['push', 'heroku', 'master'], { stdio: 'inherit' }),
-    ]);
-  } catch (err) {
-    process.exit(1);
-  }
-};
-
-/**
- * Deploy to Heroku with Docker
- *
- * @returns {Promise<void>}
- */
-
-const deployWithDocker = async () => {
-  // Commands to be executed inorder to deploy the webapp to Heroku as a Docker container.
-  try {
-    Promise.all([
-      await execa('heroku', ['login'], { stdio: 'inherit' }),
-      await execa('heroku', ['container:login'], { stdio: 'inherit' }),
-      await execa('heroku', ['create'], { stdio: 'inherit' }),
-      await execa('heroku', ['container:push', 'web'], { stdio: 'inherit' }),
-      await execa('heroku', ['container:release', 'web'], { stdio: 'inherit' }),
-      await execa('heroku', ['open'], { stdio: 'inherit' }),
-    ]);
-  } catch (err) {
-    process.exit(1);
-  }
-};
 
 /**
  * Deploy the webapp to Heroku
@@ -54,20 +13,16 @@ const deployWithDocker = async () => {
 
 const deployToHeroku = async () => {
   await Promise.all([
-    await validateInstallation('heroku'),
-    await validateInstallation('git help -g'),
+    validateInstallation('heroku'),
+    validateInstallation('git help -g'),
   ]);
 
-  const { mode } = await inquirer.prompt([
-    {
-      name: 'mode',
-      type: 'list',
-      choices: ['Deploy with Git', 'Deploy with Docker'],
-      message: 'Choose your preferred mode',
-    },
-  ]);
+  // Navigate to the client directory
+  process.chdir('client');
 
-  mode === 'Deploy with Git' ? deployWithGit() : deployWithDocker();
+  if (!fs.existsSync('./git')) {
+    await execa.shell('git init');
+  }
 };
 
 module.exports = deployToHeroku;
