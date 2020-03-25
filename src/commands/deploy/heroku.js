667f58aae90babf2694084f18f460e0204f29ca8
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-03-25 22:45:02 +0530

    fix: regression

--- src/commands/deploy/heroku.js
+++ src/commands/deploy/heroku.js
@@ -96,7 +96,7 @@ const deployToHeroku = async () => {
     'const express = require("express");',
     'const serveStatic = require("serve-static");',
     'const path = require("path");',
-    'app = express();',
+    'const app = express();',
     'app.use(serveStatic(path.join(__dirname, "dist")));',
     'const port = process.env.PORT || 80;',
     'app.listen(port);',
