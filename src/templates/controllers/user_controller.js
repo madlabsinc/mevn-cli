61a198fab1d2e025a8bc85eaebc06672bda92429
Dani Vijay <danimvijay@gmail.com>
2020-07-13 20:34:23 +0530

    feat: api enhancements (#181)
    * feat: api model enhancements, error handling
    
    * fix: delete logic fix, return updated response
    
    * fix: remove unnecessory validation error check in read
    
    * chore: remove id field for GraphQL server template
    
    * fix: add _id in graphql query, remove default db name userinfo
    
    * feat: add db name users, replace findOne with findById in graphQL
    
    Co-authored-by: jamesgeorge007 <jamesgeorge998001@gmail.com>

--- src/templates/controllers/user_controller.js
+++ src/templates/controllers/user_controller.js
@@ -2,40 +2,70 @@
 
 const User = require('../models/user_schema');
 
-const createData = (req, res, next) => {
-  User.create(req.body).then((err) => {
-    if (err) {
-      return next(err);
-    }
-    res.send('Entry created successfully!');
-  });
+const createData = (req, res) => {
+  User.create(req.body)
+    .then((data) => {
+      console.log('New User Created!', data);
+      res.status(201).json(data);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        console.error('Error Validating!', err);
+        res.status(422).json(err);
+      } else {
+        console.error(err);
+        res.status(500).json(err);
+      }
+    });
 };
 
-const readData = (req, res, next) => {
-  User.find({}, (err, user) => {
-    if (err) {
-      return next(err);
-    }
-    res.send(user);
-  });
+const readData = (req, res) => {
+  User.find()
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json(err);
+    });
 };
 
-const updateData = (req, res, next) => {
-  User.updateOne({ id: req.params.id }, { $set: req.body }, (err, user) => {
-    if (err) {
-      return next(err);
-    }
-    res.send(user);
-  });
+const updateData = (req, res) => {
+  User.findByIdAndUpdate(req.params.id, req.body, {
+    useFindAndModify: false,
+    new: true,
+  })
+    .then((data) => {
+      console.log('User updated!');
+      res.status(201).json(data);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        console.error('Error Validating!', err);
+        res.status(422).json(err);
+      } else {
+        console.error(err);
+        res.status(500).json(err);
+      }
+    });
 };
 
-const deleteData = (req, res, next) => {
-  User.deleteOne({ id: req.params.id }, (err, result) => {
-    if (err) {
-      return next(err);
-    }
-    res.send(result);
-  });
+const deleteData = (req, res) => {
+  User.findById(req.params.id)
+    .then((data) => {
+      if (!data) {
+        throw new Error('User not available');
+      }
+      return data.remove();
+    })
+    .then((data) => {
+      console.log('User removed!');
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json(err);
+    });
 };
 
 module.exports = {
--- src/templates/graphql/schema.js
+++ src/templates/graphql/schema.js
@@ -14,7 +14,7 @@ const UserSchema = require('../models/user_schema');
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: () => ({
-    id: { type: GraphQLID },
+    _id: { type: GraphQLID },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
   }),
@@ -23,7 +23,6 @@ const UserType = new GraphQLObjectType({
 const UserInputType = new GraphQLInputObjectType({
   name: 'UserInput',
   fields: () => ({
-    id: { type: GraphQLID },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
   }),
@@ -47,7 +46,7 @@ const schema = new GraphQLSchema({
           },
         },
         resolve(parent, args) {
-          return UserSchema.findOne({ id: args.id });
+          return UserSchema.findById(args.id);
         },
       },
     },
@@ -69,12 +68,13 @@ const schema = new GraphQLSchema({
       updateUser: {
         type: UserType,
         args: {
+          id: { type: new GraphQLNonNull(GraphQLID) },
           input: {
             type: new GraphQLNonNull(UserInputType),
           },
         },
-        resolve(parent, { input }) {
-          return UserSchema.updateOne({ id: input.id }, { $set: input });
+        resolve(parent, { id, input }) {
+          return UserSchema.findByIdAndUpdate(id, input, { new: true });
         },
       },
       deleteUser: {
@@ -83,7 +83,7 @@ const schema = new GraphQLSchema({
           id: { type: new GraphQLNonNull(GraphQLID) },
         },
         resolve(parent, { id }) {
-          return UserSchema.deleteOne({ id });
+          return UserSchema.findByIdAndDelete({ _id: id });
         },
       },
     },
