e452ec11924575c42499b28aa58acaae1a026d32
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-07-25 12:36:25 +0530

    fix: babel ignores starter templates

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,19 @@ const del = require('del');
 
 gulp.task('clean', () => del('lib/**', { force: true }));
 
-gulp.task('copy', () =>
+gulp.task('copy server templates', () =>
   gulp
     .src('./src/templates/server/**/*')
     .pipe(gulp.dest('./lib/templates/server')),
+);
+
+gulp.task('copy starter templates', () =>
+  gulp
+    .src('./src/templates/starter-templates/**/*')
+    .pipe(gulp.dest('./lib/templates/starter-templates')),
+);
+
+gulp.task(
+  'copy',
+  gulp.series('copy server templates', 'copy starter templates'),
 );
