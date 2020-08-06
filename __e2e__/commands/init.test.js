c876e90a809488d621d7fd3c83d2fca97445a4cc
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-08-06 10:24:43 +0530

    test: add test cases for init command

--- __e2e__/commands/init.test.js
+++ __e2e__/commands/init.test.js
@@ -1,24 +1,20 @@
 'use strict';
 
+import { run, runPromptWithAnswers } from '../../jest/helpers';
+
 import fs from 'fs';
+import { DOWN, ENTER } from 'inquirer-test';
+import path from 'path';
 
-import { run } from '../../jest/helpers';
+const SPACE = '\x20';
 
-const genPath = `${__dirname}/my-app`;
+// The project root
+const rootPath = path.join(__dirname, '..', '..');
+const genPath = path.join(rootPath, 'my-app');
 
 describe('mevn init', () => {
-  beforeEach(() => {
-    fs.mkdirSync(genPath);
-    fs.writeFileSync(`${genPath}/index.js`, '// Test');
-  });
-
-  afterEach(() => {
+  afterAll(() => {
     fs.rmdirSync(genPath, { recursive: true });
-  });
-
-  it('shows an appropriate warning if the specified directory already exists in path', () => {
-    const { stdout } = run(['init', 'my-app'], { cwd: __dirname });
-    expect(stdout).toContain('Error: Directory my-app already exists in path!');
   });
 
   it('shows an appropriate warning if multiple arguments were provided with init', () => {
@@ -26,6 +22,48 @@ describe('mevn init', () => {
     expect(stdout).toContain(
       'Error: Kindly provide only one argument as the directory name!!',
     );
+  });
+
+  it('creates a new MEVN stack webapp with the Nuxt.js starter template', async () => {
+    await runPromptWithAnswers(
+      ['init', 'my-app'],
+      [
+        `${DOWN}${DOWN}${DOWN}${ENTER}`, // Choose Nuxt.js
+        `${DOWN}${SPACE}${DOWN}${SPACE}${ENTER}`, // Opt for @nuxtjs/content module
+        `${DOWN}${ENTER}`, // Choose spa as the rendering mode
+        `${DOWN}${ENTER}`, // Choose static as the deploy target
+        `Y${ENTER}`, // Requires server directory
+      ],
+    );
+
+    const clientPath = path.join(genPath, 'client');
+    // nuxt.config.js
+    const nuxtConfig = require(path.join(clientPath, 'nuxt.config.js')).default;
+    // Check for Nuxt.js module config
+    expect(nuxtConfig).not.toHaveProperty('axios');
+    expect(nuxtConfig).toHaveProperty('content');
+    // Check for rendering mode and deploy target config
+    expect(nuxtConfig.mode).toBe('spa');
+    expect(nuxtConfig.target).toBe('static');
+
+    // .mevnrc
+    const projectConfigContent = {
+      name: 'my-app',
+      template: 'Nuxt.js',
+      modules: ['pwa', 'content'],
+      deployTarget: 'static',
+      isConfigured: false,
+    };
+
+    const projectConfig = JSON.parse(
+      fs.readFileSync(path.join(genPath, '.mevnrc')),
+    );
+    expect(projectConfig).toStrictEqual(projectConfigContent);
+  });
+
+  it('shows an appropriate warning if the specified directory already exists in path', () => {
+    const { stdout } = run(['init', 'my-app'], { cwd: rootPath });
+    expect(stdout).toContain('Error: Directory my-app already exists in path!');
   });
 
   it('shows an appropriate warning if creating an application within a non-empty path', () => {
