7833a92f0998adef3afb2c2f2a1f741401e2403e
James Kaviyil Jose <31133182+jamesjose03@users.noreply.github.com>
2020-10-21 15:23:43 +0530

    fix: windows issue for mevn init

--- src/commands/basic/init.js
+++ src/commands/basic/init.js
@@ -57,8 +57,6 @@ const showInstructions = () => {
   console.log(chalk.cyan.bold(`You're all set`));
   console.log(chalk.cyan.bold(`Now, just type in ${userCommandInstruction}`));
 
-  let removeCmd = isWin ? 'rmdir /s /q' : 'rm -rf';
-  execa.commandSync(`${removeCmd} ${path.join(projectPathRelative, '.git')}`);
   makeInitialCommit();
 };
 
