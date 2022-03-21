216500990589b48027d8138f73367dded89630ca
James George <james@deepsource.io>
2022-03-21 13:37:36 +0530

    update comment

--- src/cli.js
+++ src/cli.js
@@ -24,6 +24,7 @@ import pkg from '../package';
 
 updateNotifier({ pkg }).notify();
 
+// Suggest matching commands
 const suggestCommands = (cmd) => {
   const availableCommands = program.commands.map((c) => c._name);
 
